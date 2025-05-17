236c6ec38f8a53614322adb43ac6b733e364fd94
Viktor Kostadinoski <125513118+Itonkdong@users.noreply.github.com>
2025-05-17 19:50:42 +0200

    Update HomePage.tsx

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -3,15 +3,6 @@ import { useEffect } from 'react';
 // Lenis Scroll
 import { LenisScroll } from '../assets/utils/LenisScroll';
 
-// Cursor Component
-import Cursor from '../components/Cursor';
-
-// Navbar Component
-import NavBar from '../components/NavBar';
-
-// Menu Aside
-import MenuAside from '../components/MenuAside';
-
 // Video
 import Video from '../components/Video';
 
@@ -23,10 +14,7 @@ const HomePage = () => {
 
     return (
         <div className="h-auto overflow-x-hidden bg-white">
-            <Cursor></Cursor>
-            <NavBar></NavBar>
             <Video></Video>
-            <MenuAside></MenuAside>
         </div>
     );
 };
